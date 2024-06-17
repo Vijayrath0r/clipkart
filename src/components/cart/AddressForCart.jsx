71d21be032db427510f7aa6a9762f82629f10bfe
Vijay rathor <vijay.rathor@mindruby.com>
2024-06-17 14:47:06 +0530

    added address using API and google Maps APIs

--- src/components/cart/AddressForCart.jsx
+++ src/components/cart/AddressForCart.jsx
@@ -1,18 +1,51 @@
 import { Link } from "react-router-dom";
+import { getLocation } from "../../services/locationService";
+import useFetch from "../../hooks/useFetch";
+import { useEffect, useState } from "react";
 
 const AddressForCart = () => {
+  const { data } = useFetch("http://localhost:3002/adderess/getDefault", "POST", {
+    userId: 10,
+  });
+  console.log(data);
+
+  const [addressTitle, setaddressTitle] = useState("");
+  const [addressFull, setaddressFull] = useState("");
+  const [type, setType] = useState("");
+
+  useEffect(() => {
+    if (data && data.status === 0) {
+      getLocation()
+        .then((addressInfo) => {
+          console.log(addressInfo);
+          setaddressTitle(addressInfo.address_components[9].long_name);
+          setType("Current");
+          setaddressFull(addressInfo.formatted_address);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    } else if (data) {
+      const addressData = data.postData[0];
+      setaddressTitle(addressData.fullName + ", " + addressData.pinCode);
+      setType(addressData.type == 1 ? "Home" : "Office");
+      setaddressFull(addressData.line1 + ", " + addressData.line2 + ", " + addressData.city + ", " + addressData.state);
+    }
+  }, [data]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="flex bg-white p-5 mr-3">
       <div className="w-11/12">
         <div>
           <span>Deliver to: </span>
-          <span className="font-medium">Vijay Rathor, 458001</span>
-          <span className="ml-5 bg-gray-100 p-2 rounded">Home</span>
+          <span className="font-medium">{addressTitle}</span>
+          <span className="ml-5 bg-gray-100 p-2 rounded">{type}</span>
         </div>
         <div>
-          <span className="text-sm text-gray-600">
-            Garden ke pas, gandhi nagar,mandsaur
-          </span>
+          <span className="text-sm text-gray-600">{addressFull}</span>
         </div>
       </div>
       <div className="w-1/12">
---
+++ src/services/locationService.js
@@ -0,0 +1,49 @@
+// src/locationService.js
+import '../../public/js/jquery-3.7.1';
+
+export const getLocation = () => {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    let lat = position.coords.latitude;
+                    let long = position.coords.longitude;
+                    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyCnD13wQBFt56BL9LmSkIVQr2m6Gdyvza8`;
+
+                    $.ajax({
+                        type: "GET",
+                        url: url,
+                        dataType: "json",
+                        success: function (response) {
+                            let results = response.results;
+                            resolve(results[0]);
+                        },
+                        error: function (req, status, error) {
+                            reject('Sorry, there was an error.');
+                        }
+                    });
+                },
+                (error) => {
+                    let errorMessage = '';
+                    switch (error.code) {
+                        case error.PERMISSION_DENIED:
+                            errorMessage = "User denied the request for Geolocation.";
+                            break;
+                        case error.POSITION_UNAVAILABLE:
+                            errorMessage = "Location information is unavailable.";
+                            break;
+                        case error.TIMEOUT:
+                            errorMessage = "The request to get user location timed out.";
+                            break;
+                        case error.UNKNOWN_ERROR:
+                            errorMessage = "An unknown error occurred.";
+                            break;
+                    }
+                    reject(errorMessage);
+                }
+            );
+        } else {
+            reject("Sorry, Geolocation is not supported by your browser.");
+        }
+    });
+};
